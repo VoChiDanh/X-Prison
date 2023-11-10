99adbf2fe59b0502b7d08f6e52339337d37b7c2c
VoChiDanh <vochidanh.hs.c@gmail.com>
2023-11-10 18:26:52 +0700

    fix fortune

--- modules/core/src/test/java/dev/drawethree/xprison/utils/misc/NumberUtilsTest.java
+++ modules/core/src/test/java/dev/drawethree/xprison/utils/misc/NumberUtilsTest.java
@@ -1,9 +1,8 @@
 package dev.drawethree.xprison.utils.misc;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class NumberUtilsTest {
 

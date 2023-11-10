bff11be518c96fa068ca51c92eee890047948465
VoChiDanh <vochidanh.hs.c@gmail.com>
2023-11-10 18:20:06 +0700

    fix fortune

--- modules/core/src/main/java/dev/drawethree/xprison/enchants/model/impl/FortuneEnchant.java
+++ modules/core/src/main/java/dev/drawethree/xprison/enchants/model/impl/FortuneEnchant.java
@@ -12,15 +12,18 @@ import org.bukkit.inventory.meta.ItemMeta;
 
 import java.util.List;
 import java.util.Objects;
+import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.Collectors;
 
 public final class FortuneEnchant extends XPrisonEnchantment {
 
     private static List<CompMaterial> blackListedBlocks;
+    private static List<CompMaterial> whiteListedBlocks;
 
     public FortuneEnchant(XPrisonEnchants instance) {
         super(instance, 3);
         blackListedBlocks = plugin.getEnchantsConfig().getYamlConfig().getStringList("enchants." + id + ".Blacklist").stream().map(CompMaterial::fromString).filter(Objects::nonNull).collect(Collectors.toList());
+        whiteListedBlocks = plugin.getEnchantsConfig().getYamlConfig().getStringList("enchants." + id + ".Whitelist").stream().map(CompMaterial::fromString).filter(Objects::nonNull).collect(Collectors.toList());
     }
 
     @Override
@@ -37,7 +40,25 @@ public final class FortuneEnchant extends XPrisonEnchantment {
 
     @Override
     public void onBlockBreak(BlockBreakEvent e, int enchantLevel) {
+        if (enchantLevel > 0) {
+            Block block = e.getBlock();
+            if (isFortuneAffectedBlock(block)) {
+                e.getBlock().getDrops().forEach(itemStack -> itemStack.setAmount(calculateExtraDrops(enchantLevel)));
+            }
+        }
+    }
 
+    private boolean isFortuneAffectedBlock(Block block) {
+        // Implement logic to check if the block type is affected by Fortune
+        // Return true if this block is affected by the Fortune enchantment
+        // Example: return block.getType() == Material.DIAMOND_ORE;
+        return whiteListedBlocks.contains(CompMaterial.fromBlock(block)); // Placeholder, customize based on affected blocks
+    }
+
+    private int calculateExtraDrops(int fortuneLevel) {
+        // Implement the logic for extra drops based on Fortune level
+        // Example: return ThreadLocalRandom.current().nextInt(1, 4); // Fortune III emulation
+        return ThreadLocalRandom.current().nextInt((fortuneLevel - (fortuneLevel - 1)), (fortuneLevel + 1)); // Placeholder, adjust based on Fortune level
     }
 
     @Override
